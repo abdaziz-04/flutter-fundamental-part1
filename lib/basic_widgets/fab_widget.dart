8f69075146384257306caa80b295d8142ca110f1
abdaziz04 <abdaziz1713@gmail.com>
2024-09-27 00:28:05 +0700

    Tugas 2

--- lib/basic_widgets/fab_widget.dart
+++ lib/basic_widgets/fab_widget.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
 
-class FabWidget extends StatelessWidget {
-  const FabWidget({super.key});
+class MyFabWidget extends StatelessWidget {
+  const MyFabWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.thumb_up),
-          backgroundColor: Colors.pink,
-        ),
-      ),
+    return FloatingActionButton(
+      onPressed: () {},
+      child: const Icon(Icons.thumb_up),
+      backgroundColor: Colors.pink,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/basic_widgets/date_time_widget.dart';
+import 'package:hello_world/basic_widgets/dialog_widget.dart';
 import 'package:hello_world/basic_widgets/fab_widget.dart';
 import 'package:hello_world/basic_widgets/image_widget.dart';
-import 'package:hello_world/basic_widgets/loading_cupertino.dart';
-import 'package:hello_world/basic_widgets/text_widget.dart';
+import 'package:hello_world/basic_widgets/scaffold_widget.dart';
+import 'package:hello_world/basic_widgets/textfield_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,113 +17,69 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo - Abdul Aziz',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: const MyHomePage(title: 'Flutter - Abdul Aziz'),
-      home: const MyLayout(),
+      home: const MyHomePage(),
     );
   }
 }
 
-// PRAK 4 - ALERT
-class MyLayout extends StatelessWidget {
-  const MyLayout({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-          onPressed: () {
-            showAlertDialog(context);
-          },
-          child: Text('Show Alert')),
-    );
-  }
-}
-
-// Fungsi alert
-showAlertDialog(BuildContext context) {
-  // Setup tombol
-  Widget okButton = TextButton(
-    onPressed: () {
-      Navigator.of(context).pop(); // Close the dialog
-    },
-    child: const Text('OK'),
-  );
-
-  // Setup AlertDialog
-  AlertDialog alert = AlertDialog(
-    title: const Text('Alert'),
-    content: const Text('This is my message.'),
-    actions: [
-      okButton,
-    ],
-  );
-
-  // Show the dialog
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
-}
-
-// KODE LAMA
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  int _currentIndex = 0;
+  final List<Widget> _pages = const [
+    ScaffoldWidget(),
+    DialogWidget(),
+    TextfieldWidget(),
+    DatePickerWidget(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Flutter Widgets Demo'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.deepPurpleAccent,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Scaffold',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.warning),
+            label: 'Dialog',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.text_fields),
+            label: 'Input',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.date_range),
+            label: 'Date Picker',
+          ),
+        ],
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 50.0,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment Counter',
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
